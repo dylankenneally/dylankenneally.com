65bc4eb54d13c5f85dcab3b98967e07b93961b2c
Dylan Kenneally <dylan.kenneally@gmail.com>
2019-12-21 22:36:53 +1100

    first cut

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import profilePic from './images/profile.png';
 import './App.css';
+import { Twitter, Github, Linkedin, Contact } from 'grommet-icons';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<img src={profilePic} id="profile-pic" alt="Dylan Kenneally" />
+			<h1>Hi, I'm Dylan</h1>
+			<p>A software developer from Melbourne, Australia</p>
+			<a href="https://twitter.com/dylankenneally" target="_blank" rel="noopener noreferrer">
+				<Twitter className="external-link" />
+			</a>
+			<a href="https://github.com/dylankenneally" target="_blank" rel="noopener noreferrer">
+				<Github className="external-link" />
+			</a>
+			<a href="https://www.linkedin.com/in/dylankenneally" target="_blank" rel="noopener noreferrer">
+				<Linkedin className="external-link" />
+			</a>
+			<a href="mailto:dylan.kenneally+web-contact@gmail.com?Subject=Hi%2C%20Dylan" target="_blank" rel="noopener noreferrer">
+				<Contact className="external-link" />
+			</a>
+		</div>
+	);
 }
 
 export default App;
